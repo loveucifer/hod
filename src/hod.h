bb7dadcdc56e8e72526f6de335113bc706feae3c
loveucifer <134506987+loveucifer@users.noreply.github.com>
2026-05-11 14:19:13 +0530

    baycentric and swap functions

--- src/hod.h
+++ src/hod.h
@@ -362,6 +362,58 @@ static mat4_t mat4_look_at(Vec3_t eye, Vec3_t target, Vec3_t up) {
     return view_matrix;
 }
 
+///////////////////////////////////////
+/// swap functions ////////////////////
+///////////////////////////////////////
+
+static void int_swap(int* a , int* b ){
+    int temp = *a;
+    *a = *b;
+    *b = temp;
+
+}
+
+static void float_swap(float* a , float* b ){
+    float temp = *a;
+    *a = *b;
+    *b = temp;
+
+}
+
+
+// interpolation
+
+static float float_lerp(float a, float b , float t){
+    return  a + t * (b - a);     /// interpolation formula
+}
+
+
+
+//// baycentric weights ///
+
+static Vec3_t barycentric_weights(Vec2_t a, Vec2_t b, Vec2_t c, Vec2_t p) {
+    // Find the vectors between the vertices ABC and point p
+    Vec2_t ac = vec2_sub(c, a);
+    Vec2_t ab = vec2_sub(b, a);
+    Vec2_t ap = vec2_sub(p, a);
+    Vec2_t pc = vec2_sub(c, p);
+    Vec2_t pb = vec2_sub(b, p);
+
+    // Compute the area of the full parallegram/triangle ABC using 2D cross product
+    float area_parallelogram_abc = (ac.x * ab.y - ac.y * ab.x); // || AC x AB ||
+
+    // Alpha is the area of the small parallelogram/triangle PBC divided by the area of the full parallelogram/triangle ABC
+    float alpha = (pc.x * pb.y - pc.y * pb.x) / area_parallelogram_abc;
+
+    // Beta is the area of the small parallelogram/triangle APC divided by the area of the full parallelogram/triangle ABC
+    float beta = (ac.x * ap.y - ac.y * ap.x) / area_parallelogram_abc;
+
+    // Weight gamma is easily found since barycentric coordinates always add up to 1.0
+    float gamma = 1 - alpha - beta;
+
+    Vec3_t weights = { alpha, beta, gamma };
+    return weights;
+}
 
 
 #endif
