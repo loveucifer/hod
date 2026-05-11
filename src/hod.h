6400e3210b388a2430cbdd15502a858b7f8a8ee9
maazbilal49-maker <maazbilal49@gmail.com>
2026-05-11 20:46:15 +0300

    Update hod.h
    Code cleanup

--- src/hod.h
+++ src/hod.h
@@ -71,12 +71,12 @@ static inline float vec2_dot(Vec2_t a, Vec2_t b){
 }
 
 static inline void vec2_normalize(Vec2_t *v){
-    float length = sqrt(v->x * v->x + v->y * v->y );
+    float length = vec2_len(*v);
     v->x /= length;
     v->y /= length;
 }
 
-static inline Vec2_t vec2_new(float x,  float y){
+static inline Vec2_t vec2_new(float x, float y){
     Vec2_t result = {x,y};
     return result;
 }
@@ -92,7 +92,7 @@ static inline Vec2_t vec2_clone(Vec2_t *v){
 /////////////////////////////////////
 
 
-static Vec3_t vec3_new(float x,  float y,float z){
+static Vec3_t vec3_new(float x, float y, float z){
     Vec3_t result = {x,y,z};
     return result;
 }
@@ -103,7 +103,7 @@ static Vec3_t vec3_clone(Vec3_t *v){
 }
 
 static float vec3_len(Vec3_t v){
-    return sqrt(v.x * v.x + v.y * v.y + v.z * v.z);
+    return sqrtf(v.x * v.x + v.y * v.y + v.z * v.z);
 };
 
 static Vec3_t vec3_add(Vec3_t a, Vec3_t b) {
